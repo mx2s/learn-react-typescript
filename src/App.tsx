3872bba81ea095aed6603f55fa56e713d28e4a53
Max Kovalenko <mxss1998@yandex.ru>
2019-05-20 22:19:53 +0300

    added typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-root">
+      Running with typescript
+    </div>
+  );
+}
+
+export default App;
